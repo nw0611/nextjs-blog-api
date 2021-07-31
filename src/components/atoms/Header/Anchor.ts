d9ea3e4d87f5e400ee7f78c2ce08725e4f0c7a5f
渡邉直己 <naoki.watanabe.ge@gmail.com>
2021-07-31 12:39:59 +0900

    test

--- src/components/atoms/Header/Anchor.ts
+++ src/components/atoms/Header/Anchor.ts
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
-// const Anchor = styled.a`
-//   color: ${(props) => props.theme.colors.primary};
-//   text-decoration: none;
-// `;
-
 const Anchor = styled.a`
+  color: ${(props) => props.theme.colors.primary};
   text-decoration: none;
 `;
+
+// storybookをCIでbuildするとprimaryでエラーになるため一旦差し替え
+// const Anchor = styled.a`
+//   text-decoration: none;
+// `;
 
 export default Anchor;
