6d007dd20ed742b4b82f0194986394169e77568b
渡邉直己 <naoki.watanabe.ge@gmail.com>
2021-07-31 12:44:34 +0900

    一旦sbのbuildエラー修正

--- src/components/atoms/Summary/Anchor.ts
+++ src/components/atoms/Summary/Anchor.ts
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
+// const Anchor = styled.a`
+//   color: #3e465b;
+//   text-decoration: none;
+
+//   &:hover {
+//     color: ${(props) => props.theme.colors.primary};
+//   }
+// `;
+
+// storybookをCIでbuildするとprimaryでエラーになるため一旦差し替え
 const Anchor = styled.a`
   color: #3e465b;
   text-decoration: none;
-
-  &:hover {
-    color: ${(props) => props.theme.colors.primary};
-  }
 `;
 
 export default Anchor;
