d755e0e5a775be88637e6b31e2e6ed14f273da1c
渡邉直己 <naoki.watanabe.ge@gmail.com>
2021-07-31 12:24:26 +0900

    [fix]chromaticでstorybookのデプロイ用コマンドの追加

--- src/components/atoms/Header/Anchor.ts
+++ src/components/atoms/Header/Anchor.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
+// const Anchor = styled.a`
+//   color: ${(props) => props.theme.colors.primary};
+//   text-decoration: none;
+// `;
+
 const Anchor = styled.a`
-  color: ${(props) => props.theme.colors.primary};
   text-decoration: none;
 `;
 
--- src/components/atoms/Summary/ContinueReading.ts
+++ src/components/atoms/Summary/ContinueReading.ts
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 
+// const ContinueReading = styled.a`
+//   color: #666d71;
+//   display: inline-block;
+//   font-size: 14px;
+//   margin-top: 50px;
+//   text-decoration: none;
+
+//   &:hover {
+//     color: ${(props) => props.theme.colors.primary};
+//   }
+// `;
+
+// storybookをCIでbuildするとprimaryでエラーになるため一旦差し替え
 const ContinueReading = styled.a`
   color: #666d71;
   display: inline-block;
   font-size: 14px;
   margin-top: 50px;
   text-decoration: none;
-
-  &:hover {
-    color: ${(props) => props.theme.colors.primary};
-  }
 `;
 
 export default ContinueReading;
