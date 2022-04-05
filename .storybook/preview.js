c7c6f2cd3609269692cdb9d07f1b2097da2e1977
渡邉直己 <naoki.watanabe.ge@gmail.com>
2022-04-05 18:29:58 +0900

    ローカルでstorycapを使用してキャプチャをとる

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { withScreenshot } from 'storycap';
+
+export const decorators = [
+  withScreenshot, // Registration the decorator is required
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
